41b7e6f2d701183b1511174a5c0015d816f2faae
ObinnaTerry <terrydona90@gmail.com>
2023-06-04 15:03:47 +0200

    Feat: Adds Product Controller

---
+++ CSV Reader/Controllers/ProductsController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CSV_Reader.Entities;
+using CSV_Reader.Interfaces;
+
+namespace CSV_Reader.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepo _productRepo;
+
+        public ProductsController(IProductRepo productRepo)
+        {
+            _productRepo = productRepo;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(_productRepo.GetAll());
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var result = await _productRepo.GetByIDAsync(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+        // PUT: api/Products/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public IActionResult PutProduct(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            _productRepo.Update(product);
+            _productRepo.Save();
+
+            return NoContent();
+        }
+
+        // POST: api/Products
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public IActionResult PostProduct(Product product)
+        {
+            _productRepo.Insert(product);
+            _productRepo.Save();
+            return NoContent();
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _productRepo.GetByIDAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _productRepo.Delete(product);
+
+            return NoContent();
+        }
+
+    }
+}
