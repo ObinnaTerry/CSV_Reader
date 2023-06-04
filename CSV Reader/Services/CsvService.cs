42d46dc2430c0c67d76fe40e6339eb4b1246d5bc
ObinnaTerry <terrydona90@gmail.com>
2023-06-04 19:10:53 +0200

    Refactor: Extracts File Saving to a Different Class

--- CSV Reader/Services/CsvService.cs
+++ CSV Reader/Services/CsvService.cs
@@ -14,39 +14,24 @@ namespace CSV_Reader.Services
     public class CsvService
     {
         private readonly IProductRepo _productRepo;
-        private readonly IWebHostEnvironment _environment;
         private readonly IMapper _mapper;
 
-        public CsvService(IProductRepo productRepo, IWebHostEnvironment environment, IMapper mapper)
+        public CsvService(IProductRepo productRepo, IMapper mapper)
         {
             _productRepo = productRepo;
-            _environment = environment;
             _mapper = mapper;
         }
 
-        public void ImportItemsFromCsv(string csvFilePath)
+        public void ImportItemsFromCsv(StreamReader reader)
         {
             var config = new CsvConfiguration(CultureInfo.InvariantCulture) { Delimiter = ";", Encoding = Encoding.UTF8 };
 
-            using (var reader = new StreamReader(csvFilePath))
             using (var csv = new CsvReader(reader, config))
             {
                 var products = csv.GetRecords<ProductModel>().ToList();
 
                 _productRepo.Insert(_mapper.Map<List<Product>>(products));
                 _productRepo.Save();
-
-                var uploadsFolder = Path.Combine(_environment.WebRootPath, "uploads");
-
-                if (!Directory.Exists(uploadsFolder))
-                {
-                    Directory.CreateDirectory(uploadsFolder);
-                }
-
-                // Save the CSV file to the specified directory
-                var fileName = Path.GetFileName(csvFilePath);
-                var destinationPath = Path.Combine(uploadsFolder, fileName);
-                File.Copy(csvFilePath, destinationPath, true);
             }
         }
     }
---
+++ CSV Reader/Utils/FileHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace CSV_Reader.Utils
+{
+    public class FileHelper
+    {
+        public static async Task SaveFile(IWebHostEnvironment environment, IFormFile file)
+        {
+            var uploadsFolder = Path.Combine(environment.WebRootPath, "uploads");
+
+            if (!Directory.Exists(uploadsFolder))
+            {
+                Directory.CreateDirectory(uploadsFolder);
+            }
+
+            // Save the CSV file to the specified directory
+            var fileName = Path.GetFileName(file.FileName);
+            var destinationPath = Path.Combine(uploadsFolder, fileName);
+
+            var stream = new FileStream(destinationPath, FileMode.Create);
+            await file.CopyToAsync(stream);
+            await stream.FlushAsync();
+
+            stream.Close();
+        }
+    }
+}
