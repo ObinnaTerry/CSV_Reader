bdc2d4b7f7b958458d9c510f5a8b4387b0b33409
ObinnaTerry <terrydona90@gmail.com>
2023-06-04 17:24:35 +0200

    Feat: Ensures that Files Cannot be Uploaded More than Once

--- CSV Reader/Controllers/UploadController.cs
+++ CSV Reader/Controllers/UploadController.cs
@@ -1,4 +1,5 @@
-﻿using CSV_Reader.Services;
+﻿using CSV_Reader.Interfaces;
+using CSV_Reader.Services;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -9,19 +10,24 @@ namespace CSV_Reader.Controllers
     [ApiController]
     public class UploadController : ControllerBase
     {
-        private readonly IWebHostEnvironment _environment;
         private readonly CsvService _csvService;
+        private readonly IProductRepo _productRepo;
 
-        public UploadController(IWebHostEnvironment environment, CsvService csvService)
+        public UploadController(CsvService csvService, IProductRepo productRepo)
         {
-            _environment = environment;
             _csvService = csvService;
+            _productRepo = productRepo;
         }
 
         // POST api/<UploadController>
         [HttpPost]
         public IActionResult Post(IFormFile file)
         {
+            if(_productRepo.GetAll().Any())
+            {
+                return Conflict("A file was previously been uploaded");
+            }
+
             if (file == null || file.Length <= 0)
             {
                 return BadRequest("No file was selected for upload.");
