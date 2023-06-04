62c55e26456242e86e41cf1d12141ea26e33e749
ObinnaTerry <terrydona90@gmail.com>
2023-06-04 17:15:05 +0200

    Refactor: Creates Location FOlder if Not Exists

---
+++ CSV Reader/Models/ProductModel.cs
@@ -0,0 +1,22 @@
+﻿namespace CSV_Reader.Models
+{
+    public class ProductModel
+    {
+        public string? Hauptartikelnr { get; set; }
+        public string? Artikelname { get; set; }
+        public string? Hersteller { get; set; }
+        public string? Beschreibung { get; set; }
+        public string? Materialangaben { get; set; }
+        public string? Geschlecht { get; set; }
+        public string? Produktart { get; set; }
+        public string? Ärmel { get; set; }
+        public string? Bein { get; set; }
+        public string? Kragen { get; set; }
+        public string? Herstellung { get; set; }
+        public string? Taschenart { get; set; }
+        public string? Grammatur { get; set; }
+        public string? Material { get; set; }
+        public string? Ursprungsland { get; set; }
+        public string? Bildname { get; set; }
+    }
+}
--- CSV Reader/Services/CsvService.cs
+++ CSV Reader/Services/CsvService.cs
@@ -1,36 +1,51 @@
-﻿using CSV_Reader.Data;
+﻿using AutoMapper;
+using CSV_Reader.Data;
 using CSV_Reader.Entities;
 using CSV_Reader.Interfaces;
+using CSV_Reader.Mappings;
+using CSV_Reader.Models;
 using CsvHelper;
+using CsvHelper.Configuration;
 using System.Globalization;
+using System.Text;
 
 namespace CSV_Reader.Services
 {
     public class CsvService
     {
-        private readonly ApplicationContext _context;
-        private readonly string _fileDirectory;
         private readonly IProductRepo _productRepo;
+        private readonly IWebHostEnvironment _environment;
+        private readonly IMapper _mapper;
 
-        public CsvService(ApplicationContext context, string fileDirectory, IProductRepo productRepo)
+        public CsvService(IProductRepo productRepo, IWebHostEnvironment environment, IMapper mapper)
         {
-            _context = context;
-            _fileDirectory = fileDirectory;
             _productRepo = productRepo;
+            _environment = environment;
+            _mapper = mapper;
         }
 
         public void ImportItemsFromCsv(string csvFilePath)
         {
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture) { Delimiter = ";", Encoding = Encoding.UTF8 };
+
             using (var reader = new StreamReader(csvFilePath))
-            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            using (var csv = new CsvReader(reader, config))
             {
-                var products = csv.GetRecords<Product>().ToList();
-                _productRepo.Insert(products);
+                var products = csv.GetRecords<ProductModel>().ToList();
+
+                _productRepo.Insert(_mapper.Map<List<Product>>(products));
                 _productRepo.Save();
+
+                var uploadsFolder = Path.Combine(_environment.WebRootPath, "uploads");
+
+                if (!Directory.Exists(uploadsFolder))
+                {
+                    Directory.CreateDirectory(uploadsFolder);
+                }
 
                 // Save the CSV file to the specified directory
                 var fileName = Path.GetFileName(csvFilePath);
-                var destinationPath = Path.Combine(_fileDirectory, fileName);
+                var destinationPath = Path.Combine(uploadsFolder, fileName);
                 File.Copy(csvFilePath, destinationPath, true);
             }
         }
